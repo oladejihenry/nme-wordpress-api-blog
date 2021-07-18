238ab676609f15d741c53015a93bc50a4fde4cfc
Henry Dev <henrydev@Henrys-MacBook-Pro.local>
2021-07-18 20:29:32 +0100

    Minor Update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express()
 const port = 5000 
 
 //Static Files
-app.use(express.static('public '))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + '/public/css'))
 app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/js', express.static(__dirname + '/public/js'))
 
 //Template Engine
 app.set('views','./src/views')
@@ -19,4 +19,4 @@ const newsRouter = require('./src/routes/news')
 app.use('/', newsRouter)
 
 //Listen on port 5000
-app.listen(port, ()=>(console.log(`L istening on port ${port}`)))
+app.listen(port, ()=>(console.log(`Listening on port ${port}`)))
--- src/routes/news.js
+++ src/routes/news.js
@@ -4,16 +4,19 @@
 
 newsRouter.get('', async(req, res) => {
     try {
-      const newsAPI = await axios.get(`https://www.nme.com/wp-json/wp/v2/posts/`)
+      const newsAPI = await axios.get(`https://www.nme.com/wp-json/wp/v2/posts/?per_page=12`)
       res.render('news',{ articles  : newsAPI.data})
-    } catch (error) {
+    } catch (err) {
       if (err.response) {
+        res.render('news',{ articles  : null})
         console.log(err.response.data)
         console.log(err.response.status)
         console.log(err.response.header)
       }else if (err.request) {
+        res.render('news',{ articles  : null})
         console.log(err.request)
       }else {
+        res.render('news',{ articles  : null})
         console.log('Error', err.message)
       }
       
